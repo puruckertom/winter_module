b074cde754a8681eec039eed2ee0810d713c822f
sumathys <sumathy.sinnathamby@gmail.com>
2017-02-27 10:54:19 -0500

    test

--- winter_module.R
+++ winter_module.R
@@ -9,6 +9,7 @@ png(filename="C:/git/winter_module/Image/metabolism versus temperature.png",widt
 BW= seq(from = 0, to = 1200, by = 100)
 vo2at2=22.69*BW^-0.48
 vo2at15=4.23*BW^-0.27
-plot(BW,vo2at2,type='l',col='green',xlab="Mass(g)", ylab="Metabolism")
+plot(BW,vo2at2,type='l',col='green',xlab="Mass(g)", ylab="Metabolism [mL oxygen per g of bees per hour]")
 lines(BW,vo2at15,col='red')
+legend(1000, 2.5, legend=c("At 2oC", "At 15oC"),col=c("green", "red"),lty=1:1, cex=0.8,box.lty=0)
 dev.off
